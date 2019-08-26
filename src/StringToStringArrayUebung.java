8c7ebadd8230a866f96ad022b8f93964b805cda2
Nadja Holand <n.holand@gmx.de>
2019-08-26 15:20:08 +0200

    Methode toStringArray funktioniert nun teilweise

--- src/StringToStringArrayUebung.java
+++ src/StringToStringArrayUebung.java
@@ -1,29 +1,70 @@
 public class StringToStringArrayUebung
 {
+    //TODO fix this method, Nullpointer abfangen
+
     public String[] toStringArray(String jsonString)
     {
         jsonString = jsonString.strip();
         String[] stringArray = new String[100];
 
-        for(int i = 0; i < jsonString.length(); i++)
+        if(formPruefer(jsonString))
         {
             int firstPos = 0;
             int secondPos = 0;
             int counter = 0;
 
-            if(jsonString.charAt(i) == '\'' && firstPos != 0)
+            for(int i = 0; i < jsonString.length(); i++)
             {
-                String substring = jsonString.substring(firstPos+1, secondPos);
-                stringArray[counter] = "-"+substring;
-                firstPos = 0;
-                secondPos = 0;
-                counter++;
-            }
-            else if(jsonString.charAt(i) == '\'' && firstPos == 0)
-            {
-                firstPos = i;
+                if(jsonString.charAt(i) == '\'' && firstPos != 0)
+                {
+                    secondPos = i;
+                    String substring = jsonString.substring(firstPos+1, secondPos);
+                    stringArray[counter] = "-" + substring;
+                    firstPos = 0;
+                    counter++;
+                }
+                else if(jsonString.charAt(i) == '\'' && firstPos == 0)
+                {
+                    firstPos = i;
+                }
             }
         }
         return stringArray;
+    }
+
+    public Boolean formPruefer(String jsonString)
+    {
+        Boolean korrekteForm = true;
+        jsonString = jsonString.strip();
+        String erstesZeichen = jsonString.substring(0, 1);
+        String zweitesZeichen = jsonString.substring(jsonString.length()-1);
+        String restString = jsonString.substring(1, jsonString.length()-1);
+
+        if(!erstesZeichen.equals("[") || !zweitesZeichen.equals("]"))
+        {
+            korrekteForm = false;
+        }
+
+        else
+        {
+            int zaehler = 0;
+            for(int i = 0; i < restString.length(); i++)
+            {
+                char aktuellesZeichen = restString.charAt(i);
+                if(aktuellesZeichen == '\'')
+                {
+                    zaehler++;
+                }
+                else if(aktuellesZeichen == '[' || aktuellesZeichen == ']')
+                {
+                    korrekteForm = false;
+                }
+            }
+            if(zaehler % 2 == 1 || zaehler < 2)
+            {
+                korrekteForm = false;
+            }
+        }
+        return korrekteForm;
     }
 }
--- tests/StringToStringArrayUebungTest.java
+++ tests/StringToStringArrayUebungTest.java
@@ -9,9 +9,20 @@ public class StringToStringArrayUebungTest
     public void toStringArrayTest()
         {
             String[] stringArray = stringToStringArrayObject.toStringArray("['Null', 'Ei,ns', 'Zwei', 'Drei', 'Vier']");
-            //assertEquals("-Null -Ei,ns -Zwei -Drei -Vier", stringArray);
+            String[] stringArray2 = stringToStringArrayObject.toStringArray("[ Null Eins Zwei Drei");
+            String arrayToString = "";
+            for(String arrayEintrag : stringArray)
+            {
+                if(arrayEintrag != null)
+                {
+                    arrayEintrag.strip();
+                    arrayToString += arrayEintrag + " ";
+                }
+            }
+            assertEquals("-Null -Ei,ns -Zwei -Drei -Vier", arrayToString);
             assertEquals("-Null", stringArray[0]);
             assertEquals("-Ei,ns", stringArray[1]);
             assertEquals("-Vier", stringArray[4]);
+            //assertEquals("", stringArray2);
         }
 }
