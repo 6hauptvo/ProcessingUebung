67b1598007a03b481a94c40bcf55ccb86cd038a5
Nadja Holand <n.hauptvogl@gmx.de>
2019-08-29 15:06:26 +0200

    Passwort weiterimplementiert

--- src/Passwort.java
+++ src/Passwort.java
@@ -1,13 +1,27 @@
 public class Passwort
 {
+    private char[] _passwort;
+
     public Passwort(char[] passwort)
     {
-
+        _passwort = passwort;
     }
 
     public Boolean changePassword(char[] altesPasswort, char[] neuesPasswort)
     {
-        return false;
+        Boolean change = false;
+        if(_passwort == altesPasswort)
+        {
+            StarkesPasswortPruefer pruefer = new StarkesPasswortPruefer();
+            String npw = String.valueOf(neuesPasswort);
+            String apw = String.valueOf(altesPasswort);
+            if(pruefer.passwortpruefer(npw) && pruefer.passwortpruefer(apw))
+            {
+                _passwort = neuesPasswort;
+                change = true;
+            }
+        }
+        return change;
     }
 
     public void deletePassword(char[] password)
